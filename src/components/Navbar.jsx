253cf51d0c0a84aafdf95ccdcdc8acfbd3b63e4a
Facundo Sanchez <sanchezfacundocristian@hotmail.com>
2022-07-29 17:49:26 -0300

    finish responsive menu drop down

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import logo from '../img/ego-logo.png'
 import menuBttn from '../img/menu-bttn.png'
 import { useLocation } from 'react-router-dom'
 import '../css/Navbar.css'
+import closeBtn from '../img/close-btn.png'
 
 function Navbar() {
     let location = useLocation()
@@ -13,15 +14,85 @@ function Navbar() {
                 <div className='first-section-navbar'>
                     <a href="/"><li><img src={logo} alt="logo not found" height='40px' /></li></a>
                     <li className={`models ${location.pathname.includes('details') ? '' : 'selected'}`}>
-                        <h3>Modelos</h3>
+                        <h3 className='h3-models'>Modelos</h3>
                     </li>
                     <li className={`model-details ${location.pathname.includes('details') ? 'selected' : ''}`}>
-                        <h3>Ficha de modelo</h3>
+                        <h3 className='h3-models'>Ficha de modelo</h3>
                     </li>
                 </div>
+                <input type="checkbox" id="check"/>
                 <li className='menu-section'>
-                    <h3>Menú</h3> <img src={menuBttn} alt="icon not found"/>
+                    <label htmlFor="check"> <img src={menuBttn} alt="icon not found"/></label>
                 </li>
+                <div className="drop-down-menu-container">
+                    <div className='drop-down-menu-inner'>
+                        <div className='drop-down-child'>
+                            <div className='close-menu'>
+                                <label htmlFor='check'><h2>Cerrar</h2><img src={closeBtn} alt='img not found' height='30px'/></label> 
+                            </div>
+
+                            <li>
+                                <h3>Modelos</h3>
+                            </li>
+                            <li>
+                                <h3>Servicios y Accesorios</h3>
+                            </li>
+                            <li>
+                                <h3>Financiación</h3>
+                            </li>
+                            <li>
+                                <h3>Reviews y Comunidad</h3>
+                            </li>
+                        </div>
+                        <hr />
+                        <div className="drop-down-child">
+                            <li>
+                                <h3>Toyota Mobility Service</h3>
+                            </li>
+                            <li>
+                                <h3>Totoya Gazoo Racing</h3>
+                            </li>
+                            <li>
+                                <h3>Toyoya Híbridos</h3>
+                            </li>
+                        </div>
+                        <hr />
+
+                        <div className="drop-down-child">
+                            <li>
+                                <h3>Concesionarios</h3>
+                            </li>
+                            <li>
+                                <h3>Test Drive</h3>
+                            </li>
+                            <li>
+                                <h3>Contacto</h3>
+                            </li>
+                        </div>
+                        <hr />
+
+                        <div className="drop-down-child about">
+                            <li>
+                                <h3>Actividades</h3>
+                            </li>
+                            <li>
+                                <h3>Servicios al Cliente</h3>
+                            </li>
+                            <li>
+                                <h3>Ventas Especiales</h3>
+                            </li>
+                            <li>
+                                <h3>Innovación</h3>
+                            </li>
+                            <li>
+                                <h3>Prensa</h3>
+                            </li>
+                            <li>
+                                <h3>Acerca de...</h3>
+                            </li>
+                        </div>
+                    </div>
+                </div>
             </ul>
         </nav>
     )
